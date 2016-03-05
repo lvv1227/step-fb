3e757ac2fffefbb9461a5acb89251a26b37c1141
leonid <vasiliev.leonid@gmail.com>
2016-03-05 20:08:17 +0300

    step11u

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@ cd /etc/nginx/sites-available/
 sudo rm default
 sudo ln -s /home/box/web/etc/nginx.conf test.conf
 
-pip3 install django
-sudo apt-get uninstall gunicorn
+#sudo pip3 install django
+#sudo apt-get remove gunicorn
+#sudo pip3 install gunicorn
 
+sudo /etc/init.d/nginx start
+cd /home/box/web/ask/
+gunicorn -c ../etc/gunicorn.conf ask.wsgi
