aab890e79d91e771e93c63f9408e17ee2d17ce82
leonid <vasiliev.leonid@gmail.com>
2016-03-05 20:53:40 +0300

    u

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     #url(r'^$', include('qa.urls')),
     url(r'^login/.*', include('qa.urls')),
     url(r'^signup/.*', include('qa.urls')),
-    url(r'^question/[0-9]+/', include('qa.urls')),
+    url(r'^question/[0-9]+/.*', include('qa.urls')),
     url(r'^ask/.*', include('qa.urls')),
     url(r'^popular/.*', include('qa.urls')),
     url(r'^new/.*', include('qa.urls')),
