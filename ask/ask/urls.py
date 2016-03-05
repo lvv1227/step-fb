22eb982eadfacf5c65dff8e9484069e82b015f12
leonid <vasiliev.leonid@gmail.com>
2016-03-05 20:26:12 +0300

    u

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+
     url(r'^$', include('qa.urls')),
     url(r'^login/', include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
@@ -14,5 +12,5 @@ urlpatterns = patterns('',
     url(r'^ask/.*', include('qa.urls')),
     url(r'^popular/', include('qa.urls')),
     url(r'^new/', include('qa.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
