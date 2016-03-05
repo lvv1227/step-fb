5b0f1f2aea957b4f9193612fb1e32e9f87146e41
leonid <vasiliev.leonid@gmail.com>
2016-03-05 21:09:03 +0300

    u

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
-    url('', include('qa.urls')),
-    #url(r'^$', include('qa.urls')),
+urlpatterns = patterns('',
+    url(r'[\w\d]*', include('qa.urls')),
     url(r'^login/.*', include('qa.urls')),
     url(r'^signup/.*', include('qa.urls')),
     url(r'^question/[0-9]+/.*', include('qa.urls')),
