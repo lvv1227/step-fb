20427cf49928007c7d5ac12bcd70f9645a871293
leonid <vasiliev.leonid@gmail.com>
2016-03-09 18:09:04 +0300

    ask

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.test, name='test'),
+    url(r'^category/(\d+)/$', views.category_view),
+    url(r'article/(?P<pk>\d+)/?', views.article),
+    #url(r'^', views.test, name='test'),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,3 +4,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def category_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    pk = args[0]
+    return HttpResponse(pk+'OK')
+
+def post_detail(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    pk = kwargs['pk']
+    print('pk=',pk)
+    return HttpResponse(pk+'OK')
+
+def article(request, *args):
+    pk = args[0]
+    print(pk)
+    return HttpResponse(pk+'OK')
+
+#def post_detail(request, pk=None):
+
+#    print('pk==',pk)
+ #   return HttpResponse(pk+'OK')
