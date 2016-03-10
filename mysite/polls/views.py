7c66522251ab9429e9ce181fca891de0248aa113
leonid <vasiliev.leonid@gmail.com>
2016-03-10 15:58:11 +0300

    manual 3.5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.core.urlresolvers import reverse
 
 from .models import Question,Choice
 
@@ -20,8 +21,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
